6fd5f0794bc1486098e20fa9143c7117cd158456
joyceyyuan <yueyuan@hotmail.com>
2022-07-22 10:20:49 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+
+```
+/*----- constants -----*/
+timelimit
+movelimit
+
+/*----- app's state (variables) -----*/
+time
+move
+selectedCards=[]
+
+/*----- cached element references -----*/
+
+const cards=document.querySelectorAll.()
+
+/*----- event listeners -----*/
+document.querySelectorAll('.card')
+
+flipup{
+display picture;
+add the card to selectedCards;
+increment move counter;
+}
+flipdown{
+hide picture;
+remove the card to showed cards;
+}
+
+/*----- functions -----*/
+
+shuffle:generate random cards to random position
+
+check if two cards selected{
+    selectedCards.length=2
+}
+
+compare two selected cards{
+match:
+not match:
+}
+
+gameOver{
+compare time move with timelimit movelimit;
+check if all cards face up/matched,display gameover}
+
+```
