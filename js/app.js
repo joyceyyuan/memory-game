d4c0012d0e537bc1d3cc88fa3fab3a0bc2cd9696
joyceyyuan <yueyuan@hotmail.com>
2022-07-26 02:23:44 -0700

    Update style,create grid board with images

--- js/app.js
+++ js/app.js
@@ -1,46 +1,182 @@
-
-```
 /*----- constants -----*/
-timelimit
-movelimit
+//create cards array: 16 cards with 8 different pictures
+const cards=[
+    {
+        name:'Angela',
+        image:'img/Angela.png'
+    },
+    {
+        name:'Angela',
+        image:'img/Angela.png'
+    },
+    {   
+        name:'Dwight',
+        image:'img/Dwight.png'
+    },
+    {   
+        name:'Dwight',
+        image:'img/Dwight.png'
+    },
+    {   
+        name:'Jim',
+        image:'img/Jim.png'
+    }, 
+    {   
+        name:'Jim',
+        image:'img/Jim.png'
+    },       
+    {   
+        name:'Michael',
+        image:'img/Michael.png'
+    }, 
+    {   
+        name:'Michael',
+        image:'img/Michael.png'
+    },     
+    {   
+        name:'Oscar',
+        image:'img/Oscar.png'
+    },  
+    {   
+        name:'Oscar',
+        image:'img/Oscar.png'
+    }, 
+    {   
+        name:'Pam',
+        image:'img/Ryan.png'
+    },   
+    {   
+        name:'Pam',
+        image:'img/Ryan.png'
+    },    
+    {   
+        name:'Ryan',
+        image:'img/Ryan.png'
+    },
+    {   
+        name:'Ryan',
+        image:'img/Ryan.png'
+    },
+    {   
+        name:'Stanley',
+        image:'img/Stanley.png'
+    },
+    {   
+        name:'Stanley',
+        image:'img/Stanley.png'
+    },
+]
+
 
 /*----- app's state (variables) -----*/
-time
-move
-selectedCards=[]
+//track the state of the game
+
+// let time;may add timer/time countdown later
+let move;
+let pair;
+
+selectedCards=[];
+matchedCards=[];
+
 
 /*----- cached element references -----*/
+const scoreEls ={
+    move:document.querySelector('#move'),
+    pair:document.querySelector('#pair')
+}
 
-const cards=document.querySelectorAll.()
+const gridEl = document.querySelector('.grid');
+
+const replayBtn=document.querySelector('button')
+
+
+//create grid board with images
+function createGrid(){
+    cards.forEach(function(card){
+        const imageEl=document.createElement('img');
+        imageEl.setAttribute('src','img/OfficeLogo.png');
+        imageEl.setAttribute('id',cards.indexOf(card));
+        // console.log(cardImage);
+        gridEl.appendChild(imageEl);
+    });
+}
+createGrid();
+
+
+
+
+//Upon loading the app should:Initialize the state variables
+initiate();
+function initiate(){
+    score={
+        move:0,
+        pair:0
+    }
+    getRandomCards();
+    render();
+}
+
+//Render state variables to the page
+
+function render(){
+
+}
 
 /*----- event listeners -----*/
-document.querySelectorAll('.card')
+//handle a player clicking a square
+// document.querySelectorAll('.card')
 
-flipup{
-display picture;
-add the card to selectedCards;
-increment move counter;
-}
-flipdown{
-hide picture;
-remove the card to showed cards;
-}
 
-/*----- functions -----*/
 
-shuffle:generate random cards to random position
 
-check if two cards selected{
-    selectedCards.length=2
-}
+//handle a player clicking the replay button
 
-compare two selected cards{
-match:
-not match:
-}
+replayBtn.addEventListener('click',initiate);
+initiate();
 
-gameOver{
-compare time move with timelimit movelimit;
-check if all cards face up/matched,display gameover}
 
-```
+
+
+
+
+// function flipcard {
+// display picture;
+// add the card to selectedCards;
+// add 3d effect
+// increment move counter;
+// };
+
+// function hideCard {
+// hide picture;
+// remove the card to showed cards;
+// }
+
+// /*----- functions -----*/
+
+
+//Shuffle card array
+function getRandomCards(){
+	// create random objects in cards array
+	const randIndex = Math.floor(Math.random() * cards.length);
+	return cards[randIndex];
+};
+
+
+// function checkTwoSelected {
+// if  (selectedCards.length=2) {
+//    card.addeventListeners('click',hideCards);
+//    grid.appendChild();
+// }
+// }
+
+// compare two selected cards{
+// match:
+// not match:
+// }
+
+// gameOver{
+// compare time move with timelimit movelimit;
+// check if all cards face up/matched,display gameover}
+
+
+// accuracy 
