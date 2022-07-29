d1432df59af1690a4d4372fddca922c50437144c
joyceyyuan <yueyuan@hotmail.com>
2022-07-29 08:05:24 -0700

    delete top image which cause conflict with reset

--- js/app.js
+++ js/app.js
@@ -66,47 +66,35 @@ const cards=[
         image:'img/Stanley.png'
     },
 ]
-
 /*----- app's state (variables) -----*/
 
 let move=0;
 let pair=0;
-
 let openCards=[];
 let openCardsId=[];
 let pairedCards=[];
 
 /*----- cached element references -----*/
+
 const moveEl = document.getElementById('move');
 const pairEl = document.getElementById('pair');
 
-
-console.log(moveEl.textContent=`Move:  ${move}`);
-console.log(pairEl.textContent=`Pair found:    ${pair}`);
-
-
-
-const cardbackEl = document.querySelector('.card-back');
-// const replayBtn = document.getElementById('reset-btn');   
+const cardbackEl = document.querySelector('.card-back');  
 const replayBtn = document.querySelector('button');   
 const cardImage = document.querySelectorAll('img');
 const card = document.createElement('img');
 const message = document.getElementById('message');
 
-// const openEl=document.querySelector('cardId');
-// console.log(openEl)
-/*----- EL -----*/
+/*----- Event Listener -----*/
+
 replayBtn.addEventListener('click', reset);
 
 /*----- functions -----*/
-
 
 //Shuffle cards array
 function shuffleCards(){
     cards.sort(() => Math.random() - 0.5);
 };
-
-console.log(cards);
 
 shuffleCards();
 
@@ -116,52 +104,28 @@ function createGrid(){
         const card = document.createElement('img');
         card.setAttribute('src', 'img/OfficeLogo.png');
         card.setAttribute('id', i) ;
-        // console.log(card);
         card.addEventListener('click', flipCard);
         cardbackEl.appendChild(card);
     }  
 }
 
-console.log(cardbackEl) //all card images are within grid wih class=card back
-
-//Upon loading the app should:Initialize the state variables
-
-// function initialize(){
-//     move=0;
-//     pair=0;
-//     openCards=[];
-//     openCardsId=[];
-//     pairedCards=[];
-//     shuffleCards();
-// };
-// initialize();
-
-
-//count the moves,render it to the page
+//Count moves,render it to the page
 function moveCount(){
     move += 1;
     moveEl.innerText=`Move:  ${move}`
 }
 
 function flipCard(e){
-    // console.log(e.target); //check which card was clicked <img src='',id=''>
     let cardId=e.target.getAttribute('id'); 
     let flippedCard=e.target.classList.add('flipped');
-    // console.log(flippedCard);
-
-    // console.log(`You clicked card ${cardId}`); 
     // checkOpenCards(e);
     openCards.push(cards[cardId].name);
     openCardsId.push(cardId);
-    // console.log(openCards,openCardsId)
-
     e.target.setAttribute('src',cards[cardId].image);
     e.target.setAttribute('name',cards[cardId].name);
-
     moveCount();
     checkOpenCards(e);
 };
-
 
 
 //check if card open.If so, disable event listener: stop counting moves,etc.
@@ -169,8 +133,8 @@ function checkOpenCards(e){
     if(openCards.length === 2) {
         checkIfPair();
     } else if (openCards.length === 1){
-        message.innerText='please pick another card';
-        e.target.removeEventListener('click',flipCard);
+        message.innerText='Please pick another card';
+        // e.target.removeEventListener('click',flipCard);
     } else {
         //set to null  
         openCards=[];
@@ -181,63 +145,33 @@ function checkOpenCards(e){
 
 //check if cards open/clicked are a pair
 function checkIfPair() {
-    const cards = document.querySelectorAll('img')
-    const firstCard = openCardsId[0]
-    const secondCard = openCardsId[1]
-    
-    if(openCards[0] === openCards[1]) {
-       pairedCards.push(openCards); 
-       message.innerText='You found a pair';
-       e.target.removeEventListener('click',flipCard);
+    const cards = document.querySelectorAll('img');
+    const firstCard = openCardsId[0];
+    const secondCard = openCardsId[1];
+
+    if(firstCard === secondCard) {
+    pairedCards.push(openCards); 
+    pair+=1
+    message.innerText='You found a pair';
+    pairCount();
     } else {
-      cards[firstCard].setAttribute('src', 'img/OfficeLogo.png')
-      cards[secondCard].setAttribute('src', 'img/OfficeLogo.png')
-      message.innerText='sorry,please try again'
+    cards[firstCard].setAttribute('src', 'img/OfficeLogo.png');
+    cards[secondCard].setAttribute('src', 'img/OfficeLogo.png');
+    message.innerText='Sorry, please try again'
     }
     openCards=[];
     openCardsId=[];
-    }
+}
 
 
+//Count Pairs,render it to the page
+function pairCount(){
+    pairEl.innerText=`Pair found:    ${pair}`
+}
 
 
-
-
-// function checkIfPair(){
-    //If it's a pair, remain card open displaying front
-    // if(openCards[0] === openCards[1]){
-    //     console.log('You found a pair');
-    //     pairedCards.push(openCards); 
-    // }   
-    // // }   e.target.addEventListener('click',flipCard)
-    // openCards=[];
-    // openCardsId=[];
-
-    // const openEl=document.querySelector('.flipped');
-    // openEl.classList.remove('flipped');
-
-// e.target.setAttribute('src','img/OfficeLogo.png');
-
-
-
-    // const openEl=document.querySelector('.flipped');
-    // openEl.classList.remove('flipped');
-
-// e.target.setAttribute('src','img/OfficeLogo.png');
-
-
-//render the pairs counter to the page
-
-pairEl.innerText=`Pair found:    ${pairedCards.length/2}`
-
-
-
-//Define when game is over, check if all cards got paired
-//maybe set a time limit later
-//Or add a BGM audio, gamme over if player don't find all pairs when music finishes
 
 function gameWon(){
-    pair= pairedCards.length;
     if (pair === cards.length / 2){
         console.log(pairEl.textContent='Congratulations!You found all pairs');
     }
@@ -245,24 +179,23 @@ function gameWon(){
 
 //reset the card grid after clicking on the replay button
 function reset(){
-    shuffleCards()
-        for (let i=0; i<cards.length; i++){
-            const card = document.querySelector('img');
-            card.setAttribute('src', 'img/OfficeLogo.png');
-            card.setAttribute('id', i) ;
-            // console.log(card);
-            card.addEventListener('click', flipCard);
-            cardbackEl.appendChild(card);
-        }  
-    }
-    function initialize(){
-            move=0;
+    shuffleCards();
+    for (let i=0; i<cards.length; i++){
+        const card = document.querySelector('img');
+        card.setAttribute('src', 'img/OfficeLogo.png');
+        card.setAttribute('id', i) ;
+        card.addEventListener('click', flipCard);
+        cardbackEl.appendChild(card);
+    }  
+}
+
+function initialize(){
+    move=0;
     pair=0;
     openCards=[];
     openCardsId=[];
-   pairedCards=[];
+    pairedCards=[];
+}
 
-    }
-    
 
 createGrid();
