bda50bc42c0e1e6c07122780d2906ca5ec1d11bc
joyceyyuan <yueyuan@hotmail.com>
2022-07-28 17:57:05 -0700

    updates check match function

--- js/app.js
+++ js/app.js
@@ -89,11 +89,10 @@ console.log(pairEl.textContent=`Pair found:    ${pair}`);
 const cardbackEl = document.querySelector('.card-back');
 const replayBtn = document.querySelector('button');    
 const cardImage = document.querySelectorAll('img');
-const openEl=document.querySelector('cardId');
-console.log(openEl)
+const card = document.createElement('img');
+// const openEl=document.querySelector('cardId');
+// console.log(openEl)
 
-
-/*----- event listeners -----*/
 
 
 /*----- functions -----*/
@@ -104,7 +103,9 @@ function shuffleCards(){
     cards.sort(() => Math.random() - 0.5);
 };
 
-console.log(cards)
+console.log(cards);
+
+shuffleCards();
 
 //create card board
 function createGrid(){
@@ -112,7 +113,7 @@ function createGrid(){
         const card = document.createElement('img');
         card.setAttribute('src', 'img/OfficeLogo.png');
         card.setAttribute('id', i) ;
-        console.log(card);
+        // console.log(card);
         card.addEventListener('click', flipCard);
         cardbackEl.appendChild(card);
     }  
@@ -122,15 +123,15 @@ console.log(cardbackEl) //all card images are within grid wih class=card back
 
 //Upon loading the app should:Initialize the state variables
 
-function initialize(){
-    move=0;
-    pair=0;
-    openCards=[];
-    openCardsId=[];
-    pairedCards=[];
-    shuffleCards();
-};
-initialize();
+// function initialize(){
+//     move=0;
+//     pair=0;
+//     openCards=[];
+//     openCardsId=[];
+//     pairedCards=[];
+//     shuffleCards();
+// };
+// initialize();
 
 
 //count the moves,render it to the page
@@ -142,7 +143,11 @@ function moveCount(){
 function flipCard(e){
     console.log(e.target); //check which card was clicked <img src='',id=''>
     let cardId=e.target.getAttribute('id'); 
+    let flippedCard=e.target.classList.add('flipped');
+    console.log(flippedCard);
+
     console.log(`You clicked card ${cardId}`); 
+    checkOpenCards(e);
     openCards.push(cards[cardId].name);
     openCardsId.push(cardId);
     console.log(openCards,openCardsId)
@@ -151,66 +156,44 @@ function flipCard(e){
     e.target.setAttribute('name',cards[cardId].name);
 
     moveCount();
-    checkOpenCards();
+    checkOpenCards(e);
 };
 
 
 
-//Render state variables to the page
-
-// job is to update the view with the values of our state variables!
-// We call this funciton at the end of all our controller functions,
-// AKA the functions that are the callbacks to any event listener we defined 
-
-
-// function render(){
-
-// };
-
-
 //check if card open.If so, disable event listener: stop counting moves,etc.
-function checkOpenCards(){
+function checkOpenCards(e){
     if(openCards.length === 2) {
         checkIfPair();
     } else if (openCards.length === 1){
         console.log('please pick another card');
-        // e.target.removeEventListener('click',flipCard);// 
-
+        e.target.removeEventListener('click',flipCard);
     } else {
         //set to null  
         openCards=[];
         openCardsId=[];
     }
 };
-checkOpenCards();
+
 
 //check if cards open/clicked are a pair
 function checkIfPair(){
     //If it's a pair, remain card open displaying front
     if(openCards[0] === openCards[1]){
         console.log('You found a pair');
-        pairCount();
         pairedCards.push(openCards);    
-    } else {
-        //flip card over to show card back
-        let cardOne=openCards[0];
-        let cardTwo=openCards[1];
-
-        // let findId=cards.indexOf('cardOne');
-        // console.log(findId);
-
-        // cardOne.setAttribute('src','image/OfficeLogo.png');
-        // cardTwo.setAttribute('src','image/OfficeLogo.png');
-        console.log('Wrong.Please try again');
     }
+    openCards=[];
+    openCardsId=[];
+    const openEl=document.querySelector('.flipped');
+openEl.setAttribute('src','img/OfficeLogo.png');
+//     openEl.classList.remove('flipped');
 }
 
-//count the pairs,render it to the page
-function pairCount(){
-    pair +=1;
-    console.log(pairEl.textContent=`Pair found:    ${pair}`);
+//render the pairs counter to the page
 
-};
+console.log(pairEl.textContent=`Pair found:    ${pairedCards.length / 2}`)
+
 
 
 //Define when game is over, check if all cards got paired
@@ -218,10 +201,16 @@ function pairCount(){
 //Or add a BGM audio, gamme over if player don't find all pairs when music finishes
 
 function gameWon(){
-    if (pairCount === cards.length / 2){
+    if (pairedCards.length === cards.length / 2){
         console.log(pairEl.textContent='Congratulations!You found all pairs');
     }
 };
 
-//handle player clicking on the replay button
-replayBtn.addEventListener('click', initialize)
+//reset the card grid after clicking on the replay button
+function reset(){
+    shuffleCards();
+
+
+}
+
+replayBtn.addEventListener('click', reset());
